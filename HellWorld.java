1810968fd920e4a3812f864ea67c37a7845f0dd2
ufpscolt01 <43622133+ufpscolt01@users.noreply.github.com>
2019-10-23 11:45:12 -0500

    HellWorld.java
    aplicando una variacion en la relacion de salto de linea dentro de el string a imprimir

--- HellWorld.java
+++ HellWorld.java
@@ -2,6 +2,7 @@ class Hello{
   public static void main(String args[]){
     System.out.println("Hello beautiful world!");
     System.out.println("Hello sad world"); //Por la trsitesa
+    System.out.print("Hola beia dama\br");
   }
   
 
