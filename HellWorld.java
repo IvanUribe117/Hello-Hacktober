ea6ff96d3d13c0cfd92d8b419aa1b4319cdcf12f
IvanUribe117 <32749965+IvanUribe117@users.noreply.github.com>
2019-10-23 11:17:39 -0500

    Update HellWorld.java

--- HellWorld.java
+++ HellWorld.java
@@ -1,5 +1,8 @@
 class Hello{
   public static void main(String args[]){
     System.out.println("Hello beautiful world!");
+    System.out.println("Hello sad world"); //Por la trsitesa
   }
+  
+
 }
